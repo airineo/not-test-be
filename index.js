8483419dde646cfa8709bffe001cec46f62eddd9
airineo <alberto.irineo@gmail.com>
2019-11-18 18:49:03 -0600

    cambio en el servidor

--- index.js
+++ index.js
@@ -22,8 +22,23 @@ io.on('connection', (socket) => {
     socket.on('new-message', (message) => {
     	console.log("mensaje en socke on");
         console.log(message);
-        io.emit('new-message', message);
+        console.log("usuario en el socket ");
+        console.log(socket.username);
+        message = socket.username +": "+  message;
+        io.emit('new-message', message);  
     });
+
+    //Intentando agregar usuario:
+    socket.on('add user', (username) => {
+        console.log("en metodo add user");
+        socket.username = username;
+        console.log("agregando usuario..." + socket.username);
+        socket.broadcast.emit('user joined', {
+            username: socket.username,
+        });
+    });
+   
+
 });
 
 server.listen(port, () => {
